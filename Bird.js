52f732c9f4533941a9cd2656d4dfd72c1090db79
KaustubhKeni <66947556+KaustubhKeni@users.noreply.github.com>
2020-09-21 19:00:49 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,28 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,30,30);
+    //this.image = loadImage("sprites/bird.png");
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+
+
+ // if(this.body.speed < 3){
+   //   super.display()
+   //   }
+    //  else{
+      //  World.remove(world,this.body);
+       // push()
+     // this.Visibility=this.Visibility-5;
+    //  tint(250,this.Visibility);
+     // image(this.image,this.body.position.x,this.body.position.y,50,50);
+     // pop()
+      //}
+
+
+  }
+
+}
